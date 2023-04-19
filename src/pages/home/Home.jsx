50beb8e26427e06ccba63bda53599d2a1d4ff927
Mücahit Yıldırım <mucahityildirim@Mucahit-MacBook-Air.local>
2023-04-19 12:59:16 +0300

    add react-router-dom and some components

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./Home.scss";
+import Featured from "../../components/featured/Featured";
+import TrustedBy from "../../components/trustedBy/TrustedBy";
 
 const Home = () => {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <Featured />
+      <TrustedBy />
+    </div>
+  );
 };
 
 export default Home;
