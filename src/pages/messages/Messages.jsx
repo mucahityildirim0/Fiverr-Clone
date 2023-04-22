93781911d58572c0425c81f235ae41c256856456
Mücahit Yıldırım <mucahityildirim@Mucahit-MacBook-Air.local>
2023-04-22 18:42:03 +0300

    add list , chat and product page using table

--- src/pages/messages/Messages.jsx
+++ src/pages/messages/Messages.jsx
@@ -1,8 +1,89 @@
 import React from "react";
 import "./Messages.scss";
+import { Link } from "react-router-dom";
 
 const Messages = () => {
-  return <div className="messages">messages</div>;
+  const message = `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis, sed?`;
+
+  return (
+    <div className="messages">
+      <div className="container">
+        <div className="title">
+          <h1>Messages</h1>
+        </div>
+        <table>
+          <tr>
+            <th>Buyer</th>
+            <th>Last Message</th>
+            <th>Date</th>
+
+            <th>Action</th>
+          </tr>
+          <tr className="active">
+            <td>John Doe</td>
+            <td>{message.substring(0, 100)}...</td>
+            <td>1 day ago</td>
+            <td>
+              <button>Mark as read</button>
+            </td>
+          </tr>
+          <tr className="active">
+            <td>John Doe</td>
+            <td>{message.substring(0, 100)}...</td>
+            <td>1 day ago</td>
+            <td>
+              <button>Mark as read</button>
+            </td>
+          </tr>
+          <tr>
+            <td>John Doe</td>
+            <td>
+              <Link to="/message/123" className="link">
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td>1 day ago</td>
+          </tr>
+          <tr>
+            <td>John Doe</td>
+            <td>
+              <Link to="/message/123" className="link">
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td>1 day ago</td>
+          </tr>
+          <tr>
+            <td>John Doe</td>
+            <td>
+              <Link to="/message/123" className="link">
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td>1 day ago</td>
+          </tr>
+          <tr>
+            <td>John Doe</td>
+            <td>
+              <Link to="/message/123" className="link">
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td>1 day ago</td>
+          </tr>
+          <tr>
+            <td>John Doe</td>
+            <td>
+              <Link to="/message/123" className="link">
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td>1 day ago</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Messages;
